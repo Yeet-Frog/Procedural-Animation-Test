d7fe97eb4e7582b96fa17705ed6a2af66079e481
Yeet Frog <72714261+Yeet-Frog@users.noreply.github.com>
2021-09-05 00:59:01 -0400

    Uploading most files

---
+++ Assets/Scripts/Line_Renderer_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line_Renderer_Controller : MonoBehaviour
+{
+    public float MaxLeggLength;
+    public LineRenderer Line;
+    private Vector3 fottPosition;
+    private void Start()
+    {
+        SetFott();  
+    }
+    private void Update()
+    {
+        if (OutOfFott())
+        {
+            SetFott();
+        }
+        DrawLegg();
+    }
+
+    private bool OutOfFott()
+    {
+        return Vector3.Distance(transform.position, fottPosition) > MaxLeggLength;
+    }
+
+    private void SetFott()
+    {
+        Vector3 direction = transform.right;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, MaxLeggLength);
+
+        if (hit == false)
+        {
+            
+            return;
+        }
+        
+        fottPosition = hit.point;
+        
+    }
+    
+    private void DrawLegg()
+    {
+        Line.gameObject.SetActive(!OutOfFott());
+        Line.SetPosition(1, transform.InverseTransformPoint(fottPosition));
+    }
+    
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    //public float Speed = 0;
+    public int leggCount = 0;
+    public Rigidbody2D rb2d;
+    public int fallSpeed = 20;
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        leggCount = GameObject.FindGameObjectsWithTag("Legg").Length;
+        //rb2d.velocity = new Vector2(0.0f, 0.0f);
+        
+        
+        if (leggCount == 0)
+        {
+            rb2d.gravityScale = fallSpeed;
+        }
+        else
+        {
+            rb2d.gravityScale = 0;
+        }
+        float x = Input.GetAxis("Horizontal");
+
+        float y = Input.GetAxis("Vertical");
+
+        Move(new Vector2(x, y));
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if(direction == Vector2.zero)
+        {
+            return;
+        }
+         
+        //Debug.Log(direction);
+
+        Vector3 MoveAmount = new Vector3(direction.x, direction.y, 0) * Time.deltaTime * (leggCount + 1);
+
+        transform.position += MoveAmount;
+    }
+    
+}
